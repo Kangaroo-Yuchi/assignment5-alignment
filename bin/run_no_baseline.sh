25bfee855bc378c2913c2046842e2554d7d88b4b
Yuchi.Zhang <yuchi_zhang@apple.com>
2026-02-22 13:23:09 -0500

    fix

--- bin/run_no_baseline.sh
+++ bin/run_no_baseline.sh
@@ -1,1 +1,8 @@
-uv run python cs336_alignment/impl/grpo_train.py --checkpoint /workspace/assignment5-alignment/checkpoints/epoch_2 --loss_type no_baseline --n_steps 100 --lr 1e-5 --grad_accum_steps 64 --output_dir /workspace/assignment5-alignment/grpo_comparison
+#!/bin/bash
+uv run python cs336_alignment/impl/grpo_train.py \
+  --checkpoint /workspace/assignment5-alignment/checkpoints/epoch_2 \
+  --loss_type no_baseline \
+  --n_steps 100 \
+  --lr 1e-5 \
+  --grad_accum_steps 64 \
+  --output_dir /workspace/assignment5-alignment/grpo_comparison
